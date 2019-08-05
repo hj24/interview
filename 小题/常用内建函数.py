28a1015b69fdc4def861597d44f25c9d890965b6
hj24 <huangjian24@foxmail.com>
2019-08-05 22:49:20 +0800

    🖇 添加问答，常见内建函数

--- 小题/常用内建函数.py
+++ 小题/常用内建函数.py
@@ -1,2 +1,12 @@
 # 一行代码求 1 - 100 和
 print(sum(range(1, 101)))
+
+# filter方法求出列表所有奇数并构造新列表，a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_obj = filter(lambda i: i % 2 != 0, a)
+odd = [i for i in odd_obj]
+print(odd)
+
+# 列表推导式求列表所有奇数并构造新列表，a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd = [i for i in a if i % 2 != 0]
+print(odd)
---
+++ 问答/A&Q.md
@@ -0,0 +1,12 @@
+# 问答题
+## 正则
+1. 正则re.complie作用
+
+    将正则表达式编译成一个对象，加快速度，加以复用
+
+## 文件操作
+1. 用python删除文件和用linux命令删除文件方法
+
+    python: `os.remove`
+    
+    linux: `rm`
